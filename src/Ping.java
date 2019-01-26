c035faf1d0c9826c15af3c485c77a5047a445942
spurthinaveli98 <46319198+spurthinaveli98@users.noreply.github.com>
2019-01-26 12:27:44 +0530

    Add files via upload

---
+++ src/Ping.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Ping{
+    public static void main(String[] args) {
+
+        System.out.println("Enter the host ip");
+        Scanner sc = new Scanner(System.in);
+        String ip = sc.next();
+        System.out.println("Enter the number of times the server to be pinged");
+        int count = sc.nextInt();
+        String timeString;
+        float[] time = new float[count];
+
+        String pingCmd = "ping " + ip + " -c" + count;
+
+        try {
+            Runtime r = Runtime.getRuntime();
+            Process p = r.exec(pingCmd);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String inputLine = in.readLine(); //ignoring first line from ping output.
+            int size=count;
+
+            for (int i = 0; (inputLine = in.readLine()) != null && count >0; i++) {
+
+                    //extracting the substring containing time//
+                    timeString = inputLine.substring(inputLine.lastIndexOf('=') + 1, inputLine.lastIndexOf(' '));
+                    time[i] = Float.parseFloat(timeString);
+                    count--;
+
+            }
+            in.close();
+            sc.close();
+
+            Arrays.sort(time);//since median can be calculated only on sorted data
+            if(size%2 == 0){
+                System.out.println("\n" + "The median is " + (time[size/2]+time[(size/2)-1])/2 + " ms");
+            }
+            else{
+                System.out.println("\n" + "The median is " + time[size/2] + " ms");
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
